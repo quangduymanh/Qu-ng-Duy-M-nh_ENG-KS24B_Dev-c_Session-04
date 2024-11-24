5b720cf3e5d32e04c4e99f6d441b0119d3849506
quangduymanh <quangduymanh4@gmail.com>
2024-11-24 22:36:48 +0700

    upload bài 02

---
+++ session 04- bài 02.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+	int age;
+	printf("moi ban nhap so ban muon kiem tra:");
+	scanf("%d",&age);
+	if(age/2==0){
+		printf("so ban vua nhap la so chan:\n");
+	}else{
+		printf("so ban vua nhap la so le:\n");
+	}
+	
+	return 0;
+}
