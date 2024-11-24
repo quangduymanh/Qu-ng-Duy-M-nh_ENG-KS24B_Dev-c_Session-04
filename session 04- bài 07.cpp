ce2ac6d31d2f5796ed99b6840f428710b2a61628
quangduymanh <quangduymanh4@gmail.com>
2024-11-24 23:30:52 +0700

    Update session 04- bài 07.cpp

--- session 04- bài 07.cpp
+++ session 04- bài 07.cpp
@@ -3,7 +3,7 @@ int main() {
     int year;
     printf("Nhap mot nam: ");
     scanf("%d", &year);
-    if (year / 4 == 0){
+    if (year % 4 == 0){
         printf("%d la nam nhuan.\n", year);
     } else {
         printf("%d khong la nam nhuan.\n", year);
