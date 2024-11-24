024b15c14218e59c2e64dd7d81246086b105c394
quangduymanh <quangduymanh4@gmail.com>
2024-11-24 23:30:08 +0700

    Update session 04- bài 03.cpp

--- session 04- bài 03.cpp
+++ session 04- bài 03.cpp
@@ -3,11 +3,11 @@ int main(){
 	int age;
 	printf("nhap ban so ban muon kiem tra"\n);
 	scanf("%d",&age);
-	if(age / 3 == 0 && age / 5 == 0){
+	if(age % 3 == 0 && age % 5 == 0){
 		printf("so nay chia het cho 3 va 5"\n);
-    }else if (age / 3 == 0){
+    }else if (age % 3 == 0){
     	printf("so nay chia het cho 3"\n);
-	}else if (age / 5== 0 ){
+	}else if (age % 5== 0 ){
 		printf("so nay chia het cho 5"\n);
 	}else{
 		printf("so nay khong chia het cho ca 3 va 5"\n);
